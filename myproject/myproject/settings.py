1bbfefc90ef36b18825f298e9c60fcdb6fcc04c3
lilizcatMIWMIW <dima9better@gmail.com>
2026-07-13 21:58:15 +0100

    Fix: WhiteNoise correctement branché, retour à SQLite

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ✅ Changed to staticfile
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
